f1b7c4b6ecb376c8cb63e7e6f725d266e4c3ca20
Karim0Khalil <108357049+Karim0Khalil@users.noreply.github.com>
2022-10-10 16:02:50 +0300

    Exercise 4

---
+++ more_arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char * concat(char **array[], int size){
+    
+    for (int i = 0; i < size; i++)
+    {
+           
+    }
+    
+}
+int main(){
+
+    return 0;
+}
---
+++ my_pointers.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+char ** merge(char **array1, const int size1, char **array2, int size2)
+{
+    const int size = size1 + size2;
+    char **c;
+    c = (char **)malloc(sizeof(char *)*size);
+    for(int k = 0;k<size1;k++){
+        c[k] = (char*)malloc(sizeof(char)* (strlen(array1[k])+1));
+    }
+    int i = 0, j = 0;
+    while (i < size1 && j < size2)
+    {
+        if (**array1 < **array2)
+        {
+            c[i + j] = *array1;
+            array1++;
+            i++;
+        }
+        else
+        {
+            c[i + j] = *array2;
+            array2++;
+            j++;
+        }
+    }
+    while (i < size1)
+    {
+        c[i + j] = *array1;
+        array1++;
+        i++;
+    }
+    while (j < size2)
+    {
+        c[i + j] = *array2;
+        array2++;
+        j++;
+    }
+    return c;
+}
+int main()   
+{
+    char *array[] = {"za", "zb","zzzzc"};
+    char *array2[] = {"ab", "ac"};
+
+    const int size1 = sizeof(array) / sizeof(array[0]);
+    const int size2 = sizeof(array2) / sizeof(array2[0]);
+    const int size = size1 + size2;
+    char ** c = merge(array, size1, array2, size2);
+    for (int i = 0; i < (size1 + size2); i++)
+    {
+        printf("%s\n", *(c + i));
+    }
+    return 0;
+}
