575a1ecd0fbf752d5d3ef7c02bb525524d727506
Karim0Khalil <108357049+Karim0Khalil@users.noreply.github.com>
2022-10-10 21:08:09 +0300

    added specifications

--- my_pointers.c
+++ my_pointers.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-
-char ** merge(char **array1, const int size1, char **array2, int size2)
+// requires: 2 non-empty arrays of strings
+//effects: returns an array with alpha-numeric order sorted elements
+char **merge(char **array1, const int size1, char **array2, int size2)
 {
+    if (size1 == 0 || size2 == 0)
+    {
+        printf("Empty array!");
+    }
     const int size = size1 + size2;
     char **c;
-    c = (char **)malloc(sizeof(char *)*size);
-    for(int k = 0;k<size1;k++){
-        c[k] = (char*)malloc(sizeof(char)* (strlen(array1[k])+1));
+    c = (char **)malloc(sizeof(char *) * size);
+    for (int k = 0; k < size1; k++)
+    {
+        c[k] = (char *)malloc(sizeof(char) * (strlen(array1[k]) + 1));
     }
     int i = 0, j = 0;
     while (i < size1 && j < size2)
@@ -38,15 +44,15 @@ char ** merge(char **array1, const int size1, char **array2, int size2)
     }
     return c;
 }
-int main()   
+int main()
 {
-    char *array[] = {"za", "zb","zzzzc"};
+    char *array[] = {"za", "zb", "zzzzc"};
     char *array2[] = {"ab", "ac"};
 
     const int size1 = sizeof(array) / sizeof(array[0]);
     const int size2 = sizeof(array2) / sizeof(array2[0]);
     const int size = size1 + size2;
-    char ** c = merge(array, size1, array2, size2);
+    char **c = merge(array, size1, array2, size2);
     for (int i = 0; i < (size1 + size2); i++)
     {
         printf("%s\n", *(c + i));
